917d256efd6952c775ac76288eab9b71a3a04ac7
PHC <ragnarokcute@gmail.com>
2021-03-07 05:03:24 +0800

    feat: basic 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { Grid, Typography } from "@material-ui/core";
+
+
+export default function Page404 () {
+  return (
+    <Grid container direction="column" justify="space-between" alignItems="center">
+      <Grid item><Typography variant="h1">找不到喔～ 404</Typography></Grid>
+    </Grid>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { Button, AppBar, Toolbar, Typography, IconButton } from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
 import { makeStyles } from "@material-ui/core/styles";
-import { useAppSelector, useAppDispatch } from "game-note/shared/hooks/"
-import { decrement, increment } from "game-note/features/example/couter/counterSlice"
 import Head from "next/head"
 
 const useStyles = makeStyles((theme) => ({
@@ -19,8 +17,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home () {
   const classes = useStyles();
-  const count = useAppSelector((state) => state.counter.value)
-  const dispatch = useAppDispatch()
 
   return <>
     <Head>
@@ -28,13 +24,13 @@ export default function Home () {
     </Head>
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" className={classes.title}>
           News
         </Typography>
         <Button color="inherit">Login</Button>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   </>
